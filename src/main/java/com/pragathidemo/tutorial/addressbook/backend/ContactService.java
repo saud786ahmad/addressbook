209b61f10a35f2742a13ff2a5f116537fac1234e
saud786ahmad <111411978+saud786ahmad@users.noreply.github.com>
2022-08-29 18:25:06 +0530

    Update ContactService.java

--- src/main/java/com/pragathidemo/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/pragathidemo/tutorial/addressbook/backend/ContactService.java
@@ -16,10 +16,7 @@ import java.util.logging.Logger;
 public class ContactService {
 
     // Create dummy data by randomly combining first and last names
-    static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
-            "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
-            "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer" };
+    static String[] fnames = { "saud", "ahmed", "dilsher", "santosh", "nehal" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
@@ -34,7 +31,7 @@ public class ContactService {
 
             Random r = new Random(0);
             Calendar cal = Calendar.getInstance();
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 6; i++) {
                 Contact contact = new Contact();
                 contact.setFirstName(fnames[r.nextInt(fnames.length)]);
                 contact.setLastName(lnames[r.nextInt(fnames.length)]);
